d1dc349287d58aafb4854725d775305b2a5948d3
Mohsen Hussein <mohsenhussen8@gmail.com>
2021-11-21 17:58:47 +0200

    initialize the project & add accordion component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordian from './components/Accordian';
 
-function App() {
+const App = () => {
+  const items = [
+    {
+      title: 'What is react',
+      content: 'React is a frontend javascript framework'
+    },
+    {
+      title: 'Why use react',
+      content: 'because it is a nice library among frontend developers'
+    },
+    {
+      title: 'How do you use react react',
+      content: 'by creating components'
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordian items={items} />
     </div>
-  );
+  )
 }
 
 export default App;
