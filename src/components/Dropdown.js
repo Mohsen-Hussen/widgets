dcd804fcfd44b1a634f21d29c6e19cba3d901c63
Mohsen Hussein <mohsenhussen8@gmail.com>
2021-11-25 15:24:44 +0200

    useRef to close the list when click on it's items

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,8 @@
-import React  , { useState } from 'react';
+import React  , { useState , useEffect , useRef} from 'react';
 
 const Dropdown = ({options , selected , onSelectChange}) => {
     const [open , setOpen] = useState(false);
+    const ref = useRef();
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
             return null;
@@ -12,8 +13,31 @@ const Dropdown = ({options , selected , onSelectChange}) => {
             </div>
         );
     });
+    useEffect(() => {
+        document.body.addEventListener("click",(e) => {
+            if (ref.current.contains(e.target)) {
+                return;
+            }
+            setOpen(false);},
+            { capture: true }
+        );
+    }, []);
+    // useEffect(() => {
+    //     const onBodyClick = (event) => {
+    //         if (ref.current.contains(event.target)) {
+    //             return;
+    //         }
+    //         setOpen(false);
+    //     };
+    //     document.body.addEventListener("click", onBodyClick, { capture: true });
+    //     return () => {
+    //         document.body.removeEventListener("click", onBodyClick, {
+    //             capture: true,
+    //         });
+    //     };
+    // }, []);
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 <div 
