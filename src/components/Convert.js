9b5ff0a4bed5a08547d533b643ed2c10451e5af6
Mohsen Hussein <mohsenhussen8@gmail.com>
2021-11-28 16:18:33 +0200

    handel api request

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,11 +3,22 @@ import axios from 'axios';
 
 const Convert = ({language , text}) => {
     const [translated , setTranslated] = useState('');
+    const [debouncedText , setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        },1000);
+        return () => {
+            clearTimeout(timerId);
+        };
+    },[text]);
+
     useEffect(() => {
         const doTranslation = async () => {
             const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2' , {} , {
                 params : {
-                    q : text,
+                    q : debouncedText,
                     target : language.value,
                     key : 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 }
@@ -15,7 +26,8 @@ const Convert = ({language , text}) => {
             setTranslated(data.data.translations[0].translatedText);
         };
         doTranslation();
-    }, [language , text])
+    }, [language , debouncedText]);
+
     return (
         <div>
             <h1 className="ui header">{translated}</h1>
